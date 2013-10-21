aeea0c10985a517f2cf39583f97a7b109850f6b1
tatarhy <tat.j.kikyo@gmail.com>
2013-10-21 20:01:01 +0900

    add template.cpp

---
+++ .vim/template/template.cpp
@@ -0,0 +1,18 @@
+#include <iostream> // {{{
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <cstdio> // }}}
+
+#define rep(i,n) for(int i=0; i<n; i++)
+#define REP(i,s,n) for(int i=s; i<n; i++)
+
+using namespace std;
+
+int main() {
+  return 0;
+}
+
