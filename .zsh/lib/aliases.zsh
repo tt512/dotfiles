d329064b802db3181fbcbd2a78fe0c167d7e8e17
tatarhy <tat.j.kikyo@gmail.com>
2014-05-31 00:07:56 +0900

    add some aliases

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -3,9 +3,12 @@ alias ls='ls --color=auto'
 #alias lsa='ls -lah'
 alias ll='ls -al'
 alias la='ls -AC'
-#alias sl=ls # often screw this up
+alias sl=ls # often screw this up
 
 #alias grep="grep --color=always"
 
 #alias emacs 'emacsclient --alternate-editor="" -n'
 
+alias g++='g++ -fdiagnostics-color=auto'
+
+gvim () { command gvim --remote-silent "$@" || command gvim "$@"; }
