e0d0a4838511d8090a536863922325d48fb3e5d8
tatarhy <tat.j.kikyo@gmail.com>
2014-05-31 04:34:18 +0900

    To be support for clipboard safely

--- .zsh/lib/key-bindings.zsh
+++ .zsh/lib/key-bindings.zsh
@@ -9,15 +9,20 @@ bindkey "^n" history-beginning-search-forward-end
 #bindkey "\\ep" history-beginning-search-backward-end
 #bindkey "\\en" history-beginning-search-forward-end
 
+# Clipboard support
 function copy-line-as-kill() {
   zle kill-line
-  print -rn $CUTBUFFER | xclip
+  if type xclip &>/dev/null; then
+    print -rn $CUTBUFFER | xclip
+  fi
 }
 zle -N copy-line-as-kill
 bindkey '^k' copy-line-as-kill
 
 function paste-as-yank() {
-  CUTBUFFER=$(xclip -o)
+  if type xclip &>/dev/null; then
+    CUTBUFFER=$(xclip -o)
+  fi
   zle yank
 }
 zle -N paste-as-yank
