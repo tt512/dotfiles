c87bc7d898f9d5364afab27477d73e796d87a6a2
tatarhy <tat.j.kikyo@gmail.com>
2015-10-14 17:21:00 +0900

    fix emacs recipe

--- provision/emacs.rb
+++ provision/emacs.rb
@@ -4,15 +4,22 @@ package 'emacs' do
 end
 
 link File.expand_path('~/.emacs.d') do
-  to File.expand_path('~/dotfiles/.emacs.d')
+  to 'dotfiles/.emacs.d'
+end
+
+package 'skktools' do
+  action :install
+  user "root"
 end
 
 package 'skk-jisyo' do
   action :install
   user "root"
+  notifies :run, 'execute[generate jisyo]'
 end
 
-execute 'generate learge jisyo' do
+execute 'generate jisyo' do
+  action :nothing
   command 'skkdic-expr2 SKK-JISYO.L SKK-JISYO.propernoun SKK-JISYO.geo SKK-JISYO.jinmei > ~/SKK-JISYO.XL'
   cwd '/usr/share/skk'
 end
